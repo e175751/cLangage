57fea5ab334a32a31085e00d51968b0b25c6faaf
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-11-08 15:41:51 +0900

    calc clang

---
+++ calc/calc.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("計算式を入力してください:");
+    int num1,num2,result;
+    char mark;
+
+    scanf("%d %c %d",&num1,&mark,&num2);
+
+    switch(mark){
+        case '+':
+            result = Add(num1, num2);
+            break;
+        case '-':
+            result = Sub(num1, num2);
+            break;
+        case '*':
+            result = Mut(num1, num2);
+            break;
+        case '/':
+            result = Div(num1, num2);
+            break;
+        default:
+            printf("hoge");
+    }
+    printf("結果は:%d",result);
+    return 0;
+}
+
+int Add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int Sub(int num1, int num2) {
+    return num1 - num2;
+}
+
+int Mut(int num1, int num2) {
+    return num1 * num2;
+}
+
+int Div(int num1, int num2) {
+    if (num2 == 0) {
+        return 0;
+    }
+    return num1 / num2;
+}
